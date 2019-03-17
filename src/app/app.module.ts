846ace3fc50105a2ddd6a0d21662e14efe895e8f
Kim <joaquim.moreiraneto@gmail.com>
2019-03-16 23:22:33 -0300

    ok funciona

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProgressoComponent } from './progresso/progresso.component';
+import { PainelComponent } from './painel/painel.component';
+import { TopoComponent } from './topo/topo.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgressoComponent,
+    PainelComponent,
+    TopoComponent,
+    TentativasComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/frase.model.ts
@@ -0,0 +1,15 @@
+export class FraseModel {
+
+  public fraseEng: string;
+  public fraseBr: string;
+
+
+  constructor(fraseEng: string, fraseBr: string) {
+
+    this.fraseBr = fraseBr;
+    this.fraseEng = fraseEng;
+
+
+  }
+
+}
